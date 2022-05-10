86db1b5f1edc9a9aab08ada224a458fea054be67
James Suescun <james.steven2318@gmail.com>
2022-05-10 13:21:29 -0400

    add validation to create user login route

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-router.post("/", (req, res) => {
-    res.send('register a user');
+const User = require("../models/User");
+
+router.post("/", [ 
+    check("name", "name is required").not().isEmpty(), 
+    check("email", "include a valid email").isEmail(), 
+    check("password", "enter a password with more than 6 characters").isLength({ min: 6 })], 
+    (req, res) => {
+        const errors = validationResult( req );
+        if(!errors.isEmpty()){
+            return res.status(400).json({ success: false, errors: errors.array() });
+        }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
 
+// init middleware
+app.use(express.json({ extended: false }));
+
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
