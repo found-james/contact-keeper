4fc1d5f4755cc12eb3f4b12efa10a438f9d408a3
James Suescun <james.steven2318@gmail.com>
2022-05-10 13:01:16 -0400

    establish connection with mongoDB

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
-const PORT = process.env.PORT || 1010;
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
-app.listen(PORT, () => console.log( `live on ${PORT}`));
+const PORT = process.env.PORT || 1010;
+app.listen(PORT, connectDB);
