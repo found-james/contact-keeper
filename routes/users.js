e9256e4c06b34de456147ddfb2555e56df5cc0d2
James Suescun <james.steven2318@gmail.com>
2022-05-10 13:35:13 -0400

    users post route is able to create users

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
@@ -8,10 +9,31 @@ router.post("/", [
     check("name", "name is required").not().isEmpty(), 
     check("email", "include a valid email").isEmail(), 
     check("password", "enter a password with more than 6 characters").isLength({ min: 6 })], 
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult( req );
+
         if(!errors.isEmpty()){
             return res.status(400).json({ success: false, errors: errors.array() });
+        }
+
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email: email });
+
+            if (user){
+                return res.status(400).json({ msg: "user already exists" });
+            }
+
+            user = await User.create({ name, email, password });
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+            await user.save();
+            res.send("user saved");
+
+        } catch (err) {
+            res.status(500).json({ success: false, msg: err.message });
         }
 });
 
