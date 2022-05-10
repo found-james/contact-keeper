fd36606905e94d4be5217c094e6e7024417dd3a2
James Suescun <james.steven2318@gmail.com>
2022-05-10 16:23:47 -0400

    create navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>my app</h1>
       </div>
   );
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+export default function Navbar ({ title, icon }){
+    return (
+        <div className="navbar bg-primary">
+            <h1>
+                <i className={ icon }/> { title }
+            </h1>
+        </div>
+    );
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string
+}
+
+Navbar.defaultProps = {
+    title: "Contact Keeper",
+    icon: "fas fa-id-card-alt"
+}
