53059a7d2481d48b40f07648c04255ad67453fc8
James Suescun <james.steven2318@gmail.com>
2022-05-11 16:51:54 -0400

    create contact form

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Fragment } from "react";
 import Contacts from "../contacts/Contacts";
+import ContactForm from "../contacts/ContactForm";
+
 export default function Home () {
     return (
         <Fragment>
             <div className="grid-2">
                 <div>
-                    {/* contact form */}
+                   <ContactForm />
                 </div>
                 <div>
                     <Contacts />
