90737c60a6fe56c0b3e772366ccbab8a9d698555
James Suescun <james.steven2318@gmail.com>
2022-05-11 16:19:56 -0400

    create contacts components initialize context to pass down initialState to ContactItem

---
+++ client/src/components/contacts/ContactItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ContactItem({ contact }) {
+    const { id, name, email, phone, type } = contact;
+    
+  return (
+    <div className="card bg-light">
+        <h3 className="text-primary text-left">
+
+        </h3>
+    </div>
+  )
+}
+
+export default ContactItem
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Fragment } from "react";
-
+import Contacts from "../contacts/Contacts";
 export default function Home () {
     return (
-        <Fragment><div>Home</div></Fragment>
+        <Fragment>
+            <div className="grid-2">
+                <div>
+                    {/* contact form */}
+                </div>
+                <div>
+                    <Contacts />
+                </div>
+            </div>
+        </Fragment>
     );
 };
