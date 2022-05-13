d91eb90646a17b945fe9bf97d6a1e27c12fad338
James Suescun <james.steven2318@gmail.com>
2022-05-13 12:26:10 -0400

    invoke loadUser when Home component mounts to store logged in user in authState

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,18 @@
-import React from "react";
-import { Fragment } from "react";
+
+import { Fragment, useContext, useEffect } from "react";
 import Contacts from "../contacts/Contacts";
 import ContactForm from "../contacts/ContactForm";
 import ContactFilter from "../contacts/ContactFilter";
+import AuthContext from "../context/auth/authContext";
 
 export default function Home () {
+    const authContext = useContext(AuthContext);
+
+    useEffect(() => {
+        authContext.loadUser();
+        //eslint-disable-next-line
+    },[]);
+    
     return (
         <Fragment>
             <div className="grid-2">
