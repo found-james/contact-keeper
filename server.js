626b3e89d220fdd12d7e0641d9fcddb3ddc18d24
James Suescun <james.steven2318@gmail.com>
2022-05-16 09:42:55 -0400

    update scripts and prepare server for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
 
 // init middleware
@@ -10,6 +11,12 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// serve static assets
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")));
+}
 
 const PORT = process.env.PORT || 1010;
 app.listen(PORT, connectDB);
