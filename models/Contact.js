d27d85623addc94d5ae5797ea1f7c72673a589c9
James Suescun <james.steven2318@gmail.com>
2022-05-16 08:43:09 -0400

    add missing field to Contact model

--- models/Contact.js
+++ models/Contact.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const ContactSchema = mongoose.Schema({
 
     user: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+    name: { type: String, required: true },
     email: { type: String, required: true },
     phone: { type: String },
     type: { type: String, default: "personal" },
